b213a16ffee1c15bc9db7f322c48baef2e18bee9
Oscar van Tol <o.van.tol@betabit.nl>
2020-02-22 21:56:35 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # azure-pipelines-version-increment
+This is the code for the Azure Devops Pipines extension 'version-increment'. What it does is take a version (semver) from an input variable and bump it according to your settings. After this it will overwrite the input variable in the build definition so that its state is saved for the next run.
+
+## Example usage in a azurepipelines.yml file
+
+```
+yaml
+```
+
+
+## how to build and pack
+If you want something similar but different or just play around with the code be my guest. If you want to build it need to run some commands.
+If you want to create your own from scratch, visit [the official docs](https://docs.microsoft.com/en-us/azure/devops/extend/get-started/node?view=azure-devops).
+
+* install node
+* npm install -g tfx-cli 
+* npm run build-it
+* npm run pack-it
---
+++ version-increment-task/src/index.ts
@@ -0,0 +1,41 @@
+import tl = require('azure-pipelines-task-lib/task');
+import * as rm from 'typed-rest-client';
+import { inc } from 'semver';
+import btoa = require('btoa');
+
+
+async function run() {
+    try {
+        const versionVariable: string = tl.getInput('versionVariable', false) || 'version';
+        const dryRun: boolean = tl.getInput('dryRun', false) == 'true' || true;
+        const incrementType: string = (tl.getInput('incrementType', false) || 'patch');
+        const previewName: string =(tl.getInput('previewName', false) || 'preview');
+
+        let buildDefinitionId = tl.getVariable('System.DefinitionId');
+        let accessToken = tl.getVariable('System.AccessToken');
+        let collectionUri = tl.getVariable('System.CollectionUri');
+        let teamProject = tl.getVariable('System.TeamProject');
+
+        const client = new rm.RestClient('', collectionUri, undefined, { headers: { 'Authorization': 'Basic ' + btoa(":" + accessToken) } });
+
+        let response = await client.get(`${teamProject}/_apis/build/definitions/${buildDefinitionId}?api-version=5.0`);
+        var buildDefinition: any = response.result as any;
+        console.log(versionVariable + '=' + buildDefinition.variables[versionVariable].value);
+        console.log(`Incrementing version with: ${incrementType}`);
+        buildDefinition.variables[versionVariable].value = inc(buildDefinition.variables[versionVariable].value, incrementType as 'major' | 'minor' | 'patch' | 'prerelease', undefined, previewName);
+        console.log(versionVariable + '=' + buildDefinition.variables[versionVariable].value);
+        tl.setVariable(`${versionVariable}Next`, buildDefinition.variables[versionVariable].value);
+
+        console.log(`dryRun param is set to: ${dryRun}`);
+        if (!dryRun) {
+            console.log('Attempting to update pipeline definition...');
+            await client.replace(`${teamProject}/_apis/build/definitions/${buildDefinitionId}?api-version=5.0`, buildDefinition);
+            tl.setVariable(versionVariable, buildDefinition.variables[versionVariable].value);
+        }
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run();
